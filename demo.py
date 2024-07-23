f85ac35d94f25441adb4b92288f070c6cad030d6
usmanmaqbool <usmanmaqbool@outlook.com>
2024-07-23 11:28:49 -0400

    updates-graphvlad

--- demo.py
+++ demo.py
@@ -4,6 +4,7 @@ import torch
 import glob
 import numpy as np
 from torchvision.ops import masks_to_boxes
+from torchvision.transforms import ToPILImage
 
 
 import torchvision.transforms.functional as TF
@@ -21,7 +22,6 @@ parser.add_argument('--model', type=str, default='fast_scnn',
 parser.add_argument('--img_extn', default="jpg", help='RGB Image format')
 
 
-
 parser.add_argument('--dataset', type=str, default='citys',
                     help='dataset name (default: citys)')
 parser.add_argument('--weights-folder', default='./weights',
@@ -32,13 +32,47 @@ parser.add_argument('--input-pic', type=str,
 parser.add_argument('--data_dir', default="/home/leo/usman_ws/codes/Fast-SCNN-pytorch/test-data", help='Data directory')
 parser.add_argument('--outdir', default='./test_result', type=str,
                     help='path to save the predict result')
-
-parser.add_argument('--cpu', dest='cpu', action='store_true')
+parser.add_argument('--cropped', action='store_true', default=True)
+parser.add_argument('--cpu', dest='cpu', action='store_true', default=True)
 parser.set_defaults(cpu=False)
 
 
-
 args = parser.parse_args()
+
+def save_image(tensor_image, file_name):
+    """
+    Save a PyTorch tensor as an image file.
+
+    Args:
+    tensor_image (torch.Tensor): The image tensor to save.
+    file_name (str): The name of the file to save the image to.
+    # Example usage:
+    # save_image(batch[0], 'output_image.png')
+    """
+    # Define the inverse normalization
+    inv_normalize = transforms.Normalize(
+        mean=[-0.485/0.229, -0.456/0.224, -0.406/0.225],
+        std=[1/0.229, 1/0.224, 1/0.225]
+    )
+
+    # Move the tensor to CPU if it's on GPU
+    tensor_image = tensor_image.cpu()
+
+    # Apply the inverse normalization
+    tensor_image = inv_normalize(tensor_image)
+
+    # Clip the image to be in the range [0, 1]
+    tensor_image = torch.clamp(tensor_image, 0, 1)
+
+    # Convert from (C, H, W) to (H, W, C) and to numpy array
+    image = tensor_image.permute(1, 2, 0).numpy()
+
+    # Convert to a PIL image
+    image_pil = Image.fromarray((image * 255).astype('uint8'))  # Multiply by 255 if the image is in [0, 1] range
+
+    # Save the image
+    image_pil.save(file_name)
+
 
 def relabel(img):
     '''
@@ -92,16 +126,16 @@ def demo():
         transforms.ToTensor(),
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
     ])
-    
+
     image_list = glob.glob(args.data_dir + os.sep + '*.' + args.img_extn)
     print(image_list)
-    
+
     img = []
     for fname in image_list:
         image = Image.open(fname).convert('RGB')
         image = transform(image).to(device)
         img.append(image)
-        
+
     batch = torch.stack(img)
 
     model = get_fast_scnn(args.dataset, pretrained=True, root=args.weights_folder, map_cpu=args.cpu).to(device)
@@ -109,21 +143,19 @@ def demo():
     model.eval()
     with torch.no_grad():
         outputs = model(batch)
-        
+
     ### for stage 1 and stage 2, move to the cpu first
     pred_all_c = torch.argmax(outputs[0], 1).squeeze(0).cpu().data.numpy()
     ### for stage 3
     pred_all_g = torch.argmax(outputs[0], 1).squeeze(0)
-    
-
 
     ## Total number of images in batch
     N = batch.size(0)
-    
+
     # # Stage 1
     # ## All Predictions of 20 classes
-    # Road 0 
-    # Sidewalk 1 
+    # Road 0
+    # Sidewalk 1
     # building 2
     # wall 3
     # fence 4
@@ -144,96 +176,88 @@ def demo():
     # unknown 19
 
     ## Uncomment to save the S1 Images
-    # for jj in range(N):
-    #     ## current image image_list[jj]    
-    #     mask = get_color_pallete(pred_all[jj], args.dataset)
-    #     outname = os.path.splitext(os.path.split(image_list[jj])[-1])[0] + '-s1.png'
-    #     mask.save(os.path.join(args.outdir, outname))
-    
+    for jj in range(N):
+        ## current image image_list[jj]
+        mask = get_color_pallete(pred_all_c[jj], args.dataset)
+        outname = os.path.splitext(os.path.split(image_list[jj])[-1])[0] + '-s1.png'
+        mask.save(os.path.join(args.outdir, outname))
+
     ## Stage 2
     ## Merge Labels
     pred = relabel(pred_all_c.astype(np.uint8))
     pred_g_merge = relabel(pred_all_g)
-    
-    ## Uncomment to save the S2 Images
-    # for jj in range(N):
-    #     ## current image image_list[jj]    
-    #     mask = get_color_pallete(pred[jj], args.dataset)
-    #     outname = os.path.splitext(os.path.split(image_list[jj])[-1])[0] + '-s2.png'
-    #     mask.save(os.path.join(args.outdir, outname))
-    
-    
-    
+
+    # Uncomment to save the S2 Images
+    for jj in range(N):
+        ## current image image_list[jj]
+        mask = get_color_pallete(pred[jj], args.dataset)
+        outname = os.path.splitext(os.path.split(image_list[jj])[-1])[0] + '-s2.png'
+        mask.save(os.path.join(args.outdir, outname))
+
+    to_pil = ToPILImage()
+    NB = 5
     ## Stage 3
     ## Crop Certain Images / Masks
     ## Multiply with the mask and remove the zero paddings
     for jj in range(N):
-            all_label_mask = pred_g_merge[jj]
-            # count ids of 20 cat and dont take zero ids
-            labels, label_count = all_label_mask.unique(return_counts=True)
-            #dont consider backgroud
-            labels = labels[:-1]
-            label_count = label_count[:-1]
-            
-            masks = all_label_mask == labels[:, None, None]
-            # create box of that mask using boundries
-            regions = masks_to_boxes(masks.to(torch.float32))
-            ## scale to feature output
-            # boxes = boxes / 16
-    
-    
-    # patch_mask = torch.zeros((H, W)).cuda()
-    ## Stage 4
-    ## Save the final masks
-    # Create a binary mask for each label and apply it to the image
-    for jj in range(N): #all images
+        all_label_mask = pred_g_merge[jj]
+        # count ids of 20 cat and dont take zero ids
+        labels, label_count = all_label_mask.unique(return_counts=True)
+        
+        # dont consider backgroud
+        # labels = labels[:-1]
+        # label_count = label_count[:-1]
+
+        masks = all_label_mask == labels[:, None, None]
+        # create box of that mask using boundries
+        regions = masks_to_boxes(masks.to(torch.float32))
+        ## scale to feature output
+        # boxes = boxes / 16
+        ### Crop patches
+        boxesd = regions.to(torch.long)
+
+        ## Stage 4
+        ## Save the final masks
+        # Create a binary mask for each label and apply it to the image
+        ### copy from original image
+        image = batch[jj]
+        _, H, W = image.shape
+        rsizet = transforms.Resize((H, W))
+        sub_nodes = []
         for i, label in enumerate(labels): #all labels
             # Create a binary mask for the current label
             binary_mask = (all_label_mask == label).float()
-            ### copy from original image
-            image = batch[0]
-            # Apply the binary mask to the 3D image
-            masked_image = image * binary_mask
-
-            # # Save the masked 3D image (example using NIfTI format)
-            # masked_image_np = masked_image.cpu().numpy()
-
+            if i < len(labels)-1:
+                # Apply the binary mask to the 3D image
+                if args.cropped:
+                    # Crop the image to the bounding box of the binary mask
+                    x_min, y_min, x_max, y_max = boxesd[i]
+                    masked_image = image[:, y_min:y_max, x_min:x_max] * binary_mask[y_min:y_max, x_min:x_max]
+                    masked_image = rsizet(masked_image)              
+                else:
+                    masked_image = image * binary_mask
+                sub_nodes.append(masked_image.unsqueeze(0))
+            else:
+                # create mask with all label present except 
+                binary_mask_all = 1 - binary_mask
+                masked_image = image * binary_mask_all
             outname = os.path.splitext(os.path.split(image_list[jj])[-1])[0] + f'-s3-label{label}.png'  
-                                  
-            # Convert the masked image to a PIL image for saving
-            masked_image_pil = TF.to_pil_image(masked_image)
+            ### Save all patches
+            save_image(masked_image, os.path.join(args.outdir, outname))
+
+        if len(sub_nodes) < NB:
+            bb_x = [
+                [int(W / 4), int(H / 4), int(3 * W / 4), int(3 * H / 4)],
+                [0, 0, int(W / 3), H],
+                [0, 0, W, int(H / 3)],
+                [int(2 * W / 3), 0, W, H],
+                [0, int(2 * H / 3), W, H],
+            ]
             
-            # Save the masked image
-            masked_image_pil.save(os.path.join(args.outdir, outname))
-            
-            # # Optionally, convert the binary mask to a PIL image and save it
-            # binary_mask_pil = TF.to_pil_image(binary_mask)
-            # binary_mask_pil.save(f'binary_mask_label_{label}.png')
-
-
-
-
-
-    for Nx in range(N):
-            img_nodes = []
-            for idx in range(len(regions)):
-                if (idx == rr_boxes[b_idx] and obj_i[b_idx] > 3000 and len(img_nodes) < self.NB - 2
-                    ):
-                        patch_mask = patch_mask * 0
-                        patch_mask[single_label_mask == obj_ids[b_idx]] = 1
-                        patch_maskr = rsizet(patch_mask.unsqueeze(0))
-                        patch_maskr = patch_maskr.squeeze(0)
-                        boxesd = regions.to(torch.long)
-                        x_min, y_min, x_max, y_max = boxesd[b_idx]
-                        c_img = x[Nx][:, y_min:y_max, x_min:x_max]
-                        resultant = rsizet(c_img)
-                        img_nodes.append(resultant.unsqueeze(0))
-                        break
-    
-    
-    
-
-    
+            for i in range(len(bb_x) - len(img_nodes)):
+                x_cropped = masked_image[:, bb_x[i][1] : bb_x[i][3], bb_x[i][0] : bb_x[i][2]]
+                sub_nodes.append(rsizet(x_cropped.unsqueeze(0)))
+        aa = torch.stack(sub_nodes, 1)
 
 
 if __name__ == '__main__':
